32c8f0e3e630b4aa58d3a04e8fe42f628938b818
eureka <eureka@eurekaui-MacBookPro.local>
2023-03-01 22:16:31 +0900

    first commit

---
+++ src/main/java/org/example/AwsSdkInterceptor.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import software.amazon.awssdk.core.interceptor.Context;
+import software.amazon.awssdk.core.interceptor.ExecutionAttribute;
+import software.amazon.awssdk.core.interceptor.ExecutionAttributes;
+import software.amazon.awssdk.core.interceptor.ExecutionInterceptor;
+
+import java.util.Map;
+
+public class AwsSdkInterceptor implements ExecutionInterceptor {
+    public void beforeExecution(Context.BeforeExecution context, ExecutionAttributes executionAttributes) {
+        try {
+            System.out.println("AwsSdkInterceptor.beforeExecution");
+            Map<ExecutionAttribute<?>, Object> beforeAttributes = executionAttributes.getAttributes();
+            for (ExecutionAttribute<?> executionAttribute : beforeAttributes.keySet()) {
+                String attrName = executionAttribute.toString();
+                Object attributeVal = executionAttributes.getAttribute(executionAttribute);
+                System.out.println("beforeAttr|" + attrName + "=" + attributeVal);
+            }
+            throw new RuntimeException("고의로 발생시킨 에러입니다.");
+        } catch (Exception e) {
+            System.out.println("AwsSdkInterceptor.beforeExecution:" + e);
+        }
+    }
+
+    public void afterTransmission(Context.AfterTransmission context, ExecutionAttributes executionAttributes) {
+        try {
+            System.out.println("AwsSdkInterceptor.afterTransmission");
+            Map<ExecutionAttribute<?>, Object> beforeAttributes = executionAttributes.getAttributes();
+            for (ExecutionAttribute<?> executionAttribute : beforeAttributes.keySet()) {
+                String attrName = executionAttribute.toString();
+                Object attributeVal = executionAttributes.getAttribute(executionAttribute);
+                System.out.println("afterTransmission|" + attrName + "=" + attributeVal);
+            }
+            throw new RuntimeException("고의로 발생시킨 에러입니다.");
+        } catch (Exception e) {
+            System.out.println("AwsSdkInterceptor.afterTransmission:" + e);
+        }
+    }
+
+    public void afterExecution(Context.AfterExecution context, ExecutionAttributes executionAttributes) {
+        try {
+            System.out.println("AwsSdkInterceptor.afterExecution");
+            Map<ExecutionAttribute<?>, Object> afterAttributes = executionAttributes.getAttributes();
+            for (ExecutionAttribute<?> executionAttribute : afterAttributes.keySet()) {
+                String attrName = executionAttribute.toString();
+                Object attributeVal = executionAttributes.getAttribute(executionAttribute);
+                System.out.println("afterExecution|" + attrName + "=" + attributeVal);
+            }
+            throw new RuntimeException("고의로 발생시킨 에러입니다.");
+        } catch (Exception e) {
+            System.out.println("AwsSdkInterceptor.afterExecution:" + e);
+        }
+    }
+}
+
